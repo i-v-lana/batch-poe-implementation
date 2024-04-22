b60b7854bf8b55fb66039bce648cb7190475ca67
Ivanova Svetlana <sveta.02@mail.ru>
2024-04-22 22:38:59 +0200

    final installer

--- installer/main-unix.py
+++ installer/main-unix.py
@@ -2,7 +2,6 @@
 import subprocess
 import os
 
-
 # Define the commands
 commands = [
     "git clone https://github.com/microsoft/vcpkg",
@@ -16,19 +15,24 @@ commands = [
 include_dir = "./../../include"
 os.makedirs(include_dir, exist_ok=True)
 print(f"Downloading the required libraries and storing them into {include_dir}")
-print("The installer estimation time is 40 minutes.")
+print("The installer estimation time is 3 minutes.")
+print("Warning: Prerequisites are autoconf, developer tools, g++ >= 6 and git")
+print("Instructions for installing prerequisites:")
+print("Autoconf, MacOS with homebrew installed (if you don't have homebrew, install autoconf with another package manager) : $ brew install autoconf && brew install automake")
+print("Autoconf, Linux: $ sudo apt-get update && sudo apt-get install autoconf")
+print("Developer tools, MacOS: $ xcode-select --install")
+print("Developer tools, Debian, Ubuntu, popOS, and other Debian-based distributions: $ sudo apt-get update && sudo apt-get install build-essential tar curl zip unzip")
+print("Developer tools, CentOS: $ sudo yum install centos-release-scl && sudo yum install devtoolset-7 && scl enable devtoolset-7 bash")
 
-
-# Execute the commands
 for cmd in commands:
     try:
         process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                    universal_newlines=True, cwd=include_dir)
         for line in process.stdout:
-            print(line, end='')  # Print stdout
+            print(line, end='')
         for line in process.stderr:
-            print(line, end='')  # Print stderr
-        process.wait()  # Wait for the process to finish
+            print(line, end='')
+        process.wait()
     except subprocess.CalledProcessError as e:
         print(f"Error executing command: {cmd}")
         print(e)
--- installer/main-windows.py
+++ installer/main-windows.py
@@ -15,20 +15,21 @@ commands = [
 
 include_dir = "./../../include"
 os.makedirs(include_dir, exist_ok=True)
-print(f"Downloading the required libraries and storing them into {include_dir}")
+print(f"Downloading the required libraries and storing them into {include_dir}.")
 print("The installer estimation time is 40 minutes.")
+print("Warning: Prerequisites are Windows 7 or newer, git, Visual Studio 2015 Update 3 or greater.")
+print("Warning: You can compile the code without using Visual Studio, but then you should run CMake with Visual Studio compiler.")
 
 
-# Execute the commands
 for cmd in commands:
     try:
         process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                    universal_newlines=True, cwd=include_dir)
         for line in process.stdout:
-            print(line, end='')  # Print stdout
+            print(line, end='')
         for line in process.stderr:
-            print(line, end='')  # Print stderr
-        process.wait()  # Wait for the process to finish
+            print(line, end='')
+        process.wait()
     except subprocess.CalledProcessError as e:
         print(f"Error executing command: {cmd}")
         print(e)
