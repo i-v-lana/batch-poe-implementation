61beaf7e769fdbfea40cd04627f133adb6b62bed
ivanovsv <ivanova.svetlana.sochi@gmail.com>
2024-04-24 12:51:52 +0200

    to-do added

--- main.cpp
+++ main.cpp
@@ -49,6 +49,7 @@ int main(int argc, char *argv[]) {
     /// argv[3] - experiments number
     /// argv[4] - number of instances per experiment
     /// TODO: add arguments for the testing run
+    /// TODO: add to the cmakelists.txt the compiler flags for optimization.
     errortype error;
     runparams params = get_runparams(argc, argv, error);
     if (error.error) {
