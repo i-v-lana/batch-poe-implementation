9a01928e9de9176a48d0bdff33cfe107d74de931
ivanovsv <ivanova.svetlana.sochi@gmail.com>
2024-05-08 17:15:43 +0200

    theoretical-analysis

--- theoretical-analysis/verification_costs.py
+++ theoretical-analysis/verification_costs.py
@@ -1,4 +1,6 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 secpar = 128
 
@@ -30,12 +32,60 @@ def mult_bucket(_cnt):
     return evaluate(_cnt, optimal_k)
 
 
+def mult_graph():
+    fig, ax = plt.subplots()
+
+    ax.scatter(possible_cnt, exponents_result, marker='o', label='Random Exponents', color='blue')
+    ax.scatter(possible_cnt, hybrid_result, marker='s', label='Hybrid', color='black')
+    ax.scatter(possible_cnt, bucket_result, marker='d', label='Bucket', color='red')
+
+    ax.plot(possible_cnt, exponents_result, linestyle='-', color='blue', alpha=0.5)
+    ax.plot(possible_cnt, hybrid_result, linestyle='-', color='black', alpha=0.5)
+    ax.plot(possible_cnt, bucket_result, linestyle='-', color='red', alpha=0.5)
+
+    ax.set_xlabel('number of instances m')
+    ax.set_ylabel('verification cost')
+
+    ax.legend()
+    ax.grid(True, linestyle='--')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    plt.savefig('absolute_mult.pdf', dpi=300, bbox_inches='tight')
+
+    plt.show()
+
+def rel_mult_graph():
+    fig, ax = plt.subplots()
+
+    ax.scatter(possible_cnt, rel_exponents_result, marker='o', label='Random Exponents', color='blue')
+    ax.scatter(possible_cnt, rel_hybrid_result, marker='s', label='Hybrid', color='black')
+    ax.scatter(possible_cnt, rel_bucket_result, marker='d', label='Bucket', color='red')
+
+    ax.plot(possible_cnt, rel_exponents_result, linestyle='-', color='blue', alpha=0.5)
+    ax.plot(possible_cnt, rel_hybrid_result, linestyle='-', color='black', alpha=0.5)
+    ax.plot(possible_cnt, rel_bucket_result, linestyle='-', color='red', alpha=0.5)
+
+    ax.set_xlabel('number of instances m')
+    ax.set_ylabel('relative verification cost')
+
+    ax.legend()
+    ax.grid(True, linestyle='--')
+    ax.set_xscale('log')
+    plt.savefig('relative_mult.pdf', dpi=300, bbox_inches='tight')
+
+    plt.show()
+
+
 if __name__ == "__main__":
     possible_cnt = [10**i for i in range(2, 15)]
-    for cur_cnt in possible_cnt:
-        print("exponent", mult_exponent(cur_cnt))
-        print("hybrid", mult_hybrid(cur_cnt))
-        print("bucket", mult_bucket(cur_cnt))
 
+    exponents_result = [mult_exponent(cur_cnt) for cur_cnt in possible_cnt]
+    hybrid_result = [mult_hybrid(cur_cnt) for cur_cnt in possible_cnt]
+    bucket_result = [mult_bucket(cur_cnt) for cur_cnt in possible_cnt]
 
+    rel_exponents_result = [exponents_result[i] / exponents_result[i] for i in range(len(exponents_result))]
+    rel_hybrid_result = [hybrid_result[i] / exponents_result[i] for i in range(len(hybrid_result))]
+    rel_bucket_result = [bucket_result[i] / exponents_result[i] for i in range(len(bucket_result))]
 
+    mult_graph()
+    rel_mult_graph()
